74527fdbb14e03435e54a5efdec1b1fb57db120a
ferhatgec <ferhatgectao@gmail.com>
2020-08-28 20:44:07 +0300

    Add: Color functions.

--- src/GeminiTerm.c
+++ src/GeminiTerm.c
@@ -17,6 +17,16 @@
 	#include <X11/Xlib.h>
 #endif
 
+/* Color functions */
+#define CLR_R(x)   (((x) & 0xff0000) >> 16)
+#define CLR_G(x)   (((x) & 0x00ff00) >>  8)
+#define CLR_B(x)   (((x) & 0x0000ff) >>  0)
+#define CLR_16(x)  ((double)(x) / 0xff)
+#define CLR_GDK(x) (const GdkRGBA){ .red = CLR_16(CLR_R(x)), \
+                                    .green = CLR_16(CLR_G(x)), \
+                                    .blue = CLR_16(CLR_B(x)), \
+                                    .alpha = 0 }
+
 /*
 	TODO: Add color scheme, transparent option and version.
 */
