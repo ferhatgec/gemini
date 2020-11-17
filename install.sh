c6fb3401fc6df38e340630c40d8b6b56b49b6d7f
ferhatgec <ferhatgectao@gmail.com>
2020-11-17 19:29:53 +0300

    Fix: Install permissions.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-gcc -O2 -Wall -I./include/ -I./usr/local/include/ $(pkg-config --cflags vte-2.91) ./src/GeminiTerm.c -o /bin/gemini $(pkg-config --libs vte-2.91)
+sudo gcc -O2 -Wall -I./include/ -I./usr/local/include/ $(pkg-config --cflags vte-2.91) ./src/GeminiTerm.c -o /bin/gemini $(pkg-config --libs vte-2.91)
 
-mkdir /usr/share/pixmaps/gemini/
+sudo mkdir /usr/share/pixmaps/gemini/
 
-cp resource/*.png /usr/share/pixmaps/gemini/
+sudo cp resource/*.png /usr/share/pixmaps/gemini/
 
-cp gemini.desktop /usr/share/applications/
+sudo cp gemini.desktop /usr/share/applications/
